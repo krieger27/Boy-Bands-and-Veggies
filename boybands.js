fe7ab39a1801a70e58e681c087cd1c2774359efb
krieger27 <krieger@vols.utk.edu>
2017-11-28 17:29:27 -0600

    finished

---
+++ boybands.js
@@ -0,0 +1,32 @@
+
+
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// The number of loops to perform (what if the array changes?)
+var loopCount = 5;
+
+// Keep track of which band we're on in the loop
+var currentBand = "";
+
+// Keep track of which veggie we're on in the loop
+var currentVeggie = "";
+
+// Get a reference to the appropriate DOM element for bands
+var bandElement = document.getElementById("boy-bands");
+
+// Get a reference to the appropriate DOM element for vegetables
+var veggieElement = document.getElementById("vegetables");
+
+// Start looping
+  
+  // Add the band names into the correct <div>
+  for (var i=0; i< bands.length;i++) {
+    bandElement.innerHTML += "<h2>" + bands[i] +"</h2>";
+  }
+  
+  // Add the veggie names into the correct <div>
+  for (var i=0; i<vegetables.length; i++) {
+    veggieElement.innerHTML += "<h2>" + vegetables[i] +"</h2>";
+
+}
